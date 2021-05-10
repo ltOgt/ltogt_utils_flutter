b6afa07703403c88b7f0624b0e3678f98ed68476
ltogt <omnesiaorg@gmail.com>
2021-05-10 15:53:05 +0200

    Expose state

--- lib/ltogt_utils_flutter.dart
+++ lib/ltogt_utils_flutter.dart
@@ -5,3 +5,5 @@ export 'src/widget/future/future_switch.dart';
 export 'src/widget/post_frame/render_helper.dart';
 export 'src/widget/post_frame/post_frame_builder.dart';
 export 'src/widget/responsive/row_or_column.dart';
+
+export 'src/state/model/stateful_model.dart';
