f468420467603a516fdcae69c85d9ef1909d2685
ltogt <omnesiaorg@gmail.com>
2021-08-13 23:01:22 +0200

    expose render object and postframe

--- lib/src/widget/post_frame/render_helper.dart
+++ lib/src/widget/post_frame/render_helper.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/widgets.dart';
 
 class RenderHelper {
+  /// Add a callback to be executed on the next frame.
+  ///
+  /// This can be useful if you need to call e.g. [RenderHelper.getSize]
+  ///
+  /// Returns true if sucessfull, false if [WidgetsBinding.instance] is null.
+  static bool addPostFrameCallback(Function(Duration) callback) {
+    final instance = WidgetsBinding.instance;
+    if (instance == null) return false;
+
+    instance.addPostFrameCallback(callback);
+    return true;
+  }
+
   /// Get the global size (flutter window) of a widget with key=[globalKey].
   ///
   /// Returns null if the widget has no context or no RenderBox.
@@ -53,5 +66,9 @@ class RenderHelper {
     }
 
     return renderObject.localToGlobal(Offset.zero) & renderObject.size;
+  }
+
+  static RenderObject? getRenderObject({required GlobalKey globalKey}) {
+    return globalKey.currentContext?.findRenderObject();
   }
 }
