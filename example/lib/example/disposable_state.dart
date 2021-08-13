ed9a4e0a1fc1e220e1bf550acb5fb80d66711289
ltogt <omnesiaorg@gmail.com>
2021-08-13 15:38:13 +0200

    add option to pass existing value instead of init

--- example/lib/example/disposable_state.dart
+++ example/lib/example/disposable_state.dart
@@ -9,7 +9,18 @@ class DisposableStateExample extends StatelessWidget {
     return Theme(
       data: ThemeData.dark(),
       child: Scaffold(
-        body: Center(child: ScrollContent()),
+        body: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ScrollContent(),
+              SizedBox(
+                width: 20,
+              ),
+              ScrollContentWithController(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -22,9 +33,9 @@ class ScrollContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DisposableStateBuilder(
+    return SmallStateBuilder(
         stateMap: {
-          "sc": DisposableStateDefaults.scrollController(onChange: (c) => print(c)),
+          "sc": SmallStateDefaults.scrollController(onChange: (c) => print(c)),
           // "sc": DisposableState<ScrollController>(
           //   init: () => ScrollController(),
           //   addListener: (sc) {
@@ -34,6 +45,49 @@ class ScrollContent extends StatelessWidget {
           //   removeListener: (sc) => sc.removeListener(listen),
           //   dispose: (sc) => sc.dispose(),
           // ),
+        },
+        build: (context, stateMap) {
+          return SingleChildScrollView(
+            controller: stateMap["sc"]!.value,
+            child: Column(
+              children: [
+                for (int i = 0; i < 100; i++)
+                  Container(
+                    height: 50,
+                    width: 300,
+                    color: (i % 2 == 0) ? Colors.black : Colors.blue,
+                    child: Text("$i"),
+                  ),
+              ],
+            ),
+          );
+        });
+  }
+}
+
+class ScrollContentWithController extends StatefulWidget {
+  ScrollContentWithController({Key? key}) : super(key: key);
+
+  @override
+  State<ScrollContentWithController> createState() => _ScrollContentWithControllerState();
+}
+
+class _ScrollContentWithControllerState extends State<ScrollContentWithController> {
+  ScrollController ctrl = ScrollController();
+  @override
+  void dispose() {
+    ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SmallStateBuilder(
+        stateMap: {
+          "sc": SmallStateDefaults.scrollController(
+            value: ctrl,
+            onChange: (c) => print(c),
+          ),
         },
         build: (context, stateMap) {
           return SingleChildScrollView(
--- lib/src/widget/builder/disposable_state.dart
+++ lib/src/widget/builder/disposable_state.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/widgets.dart';
 
-class DisposableState<T> {
+class SmallState<T> {
   /// Value of the disposable.
+  ///
+  /// If [value] is provided (non-null) in constructor, [init] and [dispose] are never called.
+  ///
+  /// If [value] is not provided (null) in constructor, [init] and [dispose] are called to create and tear down object.
   /// Non-Null after init has been called, and before decode has been called.
-  /// E.g. Inside [build] of [DisposableStateBuilder]
+  /// E.g. Inside [build] of [SmallStateBuilder]
   T? value;
 
-  /// Called during init in [DisposableStateBuilder]
-  T Function() init;
+  /// Called during init in [SmallStateBuilder] iff [value] is not provided.
+  ///
+  /// Required iff [value] is null.
+  T Function()? init;
 
   /// Add a listener to [T]
   void Function(T)? addListener;
@@ -20,19 +26,50 @@ class DisposableState<T> {
   _addListenerInternal<TI>(TI ti) => addListener?.call(ti as T);
   _removeListenerInternal<TI>(TI ti) => removeListener?.call(ti as T);
 
-  /// Tear down of [T]
-  void Function(T) dispose;
+  /// Tear down of [T] created with [init] iff [value] is not provided.
+  ///
+  /// Required iff [value] is null.
+  void Function(T)? dispose;
 
-  DisposableState({
+  factory SmallState.value({
+    required T value,
+    Function(T)? addListener,
+    Function(T)? removeListener,
+  }) =>
+      SmallState(
+        value: value,
+        init: null,
+        dispose: null,
+        addListener: addListener,
+        removeListener: removeListener,
+      );
+
+  factory SmallState.create({
+    required T Function() init,
+    required Function(T) dispose,
+    Function(T)? addListener,
+    Function(T)? removeListener,
+  }) =>
+      SmallState(
+        value: null,
+        init: init,
+        dispose: dispose,
+        addListener: addListener,
+        removeListener: removeListener,
+      );
+
+  SmallState({
+    required this.value,
     required this.init,
     this.addListener,
     this.removeListener,
     required this.dispose,
-  }) : assert((removeListener == null) == (addListener == null));
+  })  : assert((removeListener == null) == (addListener == null), "Must removeListener iff addListener"),
+        assert((value == null) != (init == null && dispose == null), "Must pass value XOR init & dispose");
 }
 
-class DisposableStateBuilder extends StatefulWidget {
-  DisposableStateBuilder({
+class SmallStateBuilder extends StatefulWidget {
+  SmallStateBuilder({
     Key? key,
     required this.stateMap,
     required this.build,
@@ -40,24 +77,26 @@ class DisposableStateBuilder extends StatefulWidget {
 
   final Widget Function(
     BuildContext context,
-    Map<dynamic, DisposableState> stateMap,
+    Map<dynamic, SmallState> stateMap,
   ) build;
 
-  /// Add [DisposableState] to be used by the builder.
+  /// Add [SmallState] to be used by the builder.
   ///
   /// The key used to retreive the state again can be anything
   /// - (must support lookup via equals/hash)
-  final Map<dynamic, DisposableState> stateMap;
+  final Map<dynamic, SmallState> stateMap;
 
   @override
-  _DisposableStateBuilderState createState() => _DisposableStateBuilderState();
+  _SmallStateBuilderState createState() => _SmallStateBuilderState();
 }
 
-class _DisposableStateBuilderState extends State<DisposableStateBuilder> {
+class _SmallStateBuilderState extends State<SmallStateBuilder> {
   @override
   void initState() {
     for (final state in widget.stateMap.values) {
-      state.value = state.init();
+      if (state.init != null) {
+        state.value = state.init!();
+      }
       state._addListenerInternal(state.value);
     }
     super.initState();
@@ -67,7 +106,10 @@ class _DisposableStateBuilderState extends State<DisposableStateBuilder> {
   void dispose() {
     for (final state in widget.stateMap.values) {
       state._removeListenerInternal(state.value);
-      state.dispose(state.value);
+      if (state.dispose != null) {
+        state.dispose!(state.value);
+      }
+
       state.value = null;
     }
     super.dispose();
@@ -79,27 +121,45 @@ class _DisposableStateBuilderState extends State<DisposableStateBuilder> {
   }
 }
 
-class DisposableStateDefaults {
-  static DisposableState<T> changeNotifier<T extends ChangeNotifier>({
-    required T Function() init,
+class SmallStateDefaults {
+  /// Use [value] or create new [T] to be used via [SmallStateBuilder].
+  /// Optionally add a listener.
+  static SmallState<T> changeNotifier<T extends ChangeNotifier>({
+    T Function()? init,
+    T? value,
     void Function(T t)? onChange,
   }) {
-    final t = init();
-    void _onChange() => onChange!.call(t);
+    assert((init == null) != (value == null), "Provide either init or value");
 
-    void _addListener<T>() => t.addListener(_onChange);
-    void _removeListener<T>() => t.removeListener(_onChange);
+    bool useValue = value != null;
 
-    return DisposableState(
-      init: () => t,
+    final _t = useValue ? value : init!();
+    final _init = useValue ? null : () => _t;
+    final _disp = useValue ? null : (T _) => _t.dispose();
+
+    void _onChange() => onChange!.call(_t);
+
+    void _addListener<T>() => _t.addListener(_onChange);
+    void _removeListener<T>() => _t.removeListener(_onChange);
+
+    return SmallState(
+      value: useValue ? value : null,
+      init: _init,
       addListener: onChange == null ? null : (_) => _addListener(),
       removeListener: onChange == null ? null : (_) => _removeListener(),
-      dispose: (_) => t.dispose(),
+      dispose: _disp,
     );
   }
 
-  static DisposableState<ScrollController> scrollController({
+  /// Use [value] or create new [ScrollController] to be used via [SmallStateBuilder].
+  /// Optionally add a listener.
+  static SmallState<ScrollController> scrollController({
     void Function(ScrollController ctrl)? onChange,
+    ScrollController? value,
   }) =>
-      DisposableStateDefaults.changeNotifier(init: () => ScrollController(), onChange: onChange);
+      SmallStateDefaults.changeNotifier(
+        value: value,
+        init: value != null ? null : () => ScrollController(),
+        onChange: onChange,
+      );
 }
