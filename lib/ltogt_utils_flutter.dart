a6365252a35c2c14d7e92732408d26de28f4aad8
ltOgt <omnesiaorg@gmail.com>
2022-08-13 21:08:42 +0200

    reorder exports

--- lib/ltogt_utils_flutter.dart
+++ lib/ltogt_utils_flutter.dart
@@ -1,10 +1,12 @@
 library ltogt_utils_flutter;
 
+export 'package:ltogt_utils/ltogt_utils.dart';
+
 export 'src/widget/builder/conditional_parent_widget.dart';
 export 'src/widget/builder/change_notifier_builder.dart';
 export 'src/widget/builder/widget_list_generator.dart';
-export 'src/widget/builder/small_state.dart';
 export 'src/widget/builder/hover_builder.dart';
+export 'src/widget/builder/cached_builder.dart';
 
 export 'src/widget/future/future_switch.dart';
 
@@ -13,27 +15,24 @@ export 'src/widget/post_frame/post_frame_builder.dart';
 
 export 'src/widget/responsive/row_or_column.dart';
 
-export 'src/state/model/stateful_model.dart';
-export 'src/state/component/state_component.dart';
-export 'src/state/component/scroll_component.dart';
-
-export 'src/widget/size_helper.dart';
-
-export 'package:ltogt_utils/ltogt_utils.dart';
-
-export 'src/device/device_type.dart';
-
 export 'src/widget/paint/line_widget.dart';
 
 export 'src/widget/example/exampleLauncher.dart';
 
-export 'src/widget/builder/cached_builder.dart';
-
 export 'src/widget/text/auto_size_text.dart';
 export 'src/widget/text/measure_text.dart';
 
+export 'src/widget/size_helper.dart';
+
 export 'src/widget/file_tree_widget.dart';
 
-export 'src/color_helper.dart';
-
 export 'src/widget/overlay_manager.dart';
+
+export 'src/state/model/stateful_model.dart';
+
+export 'src/state/component/state_component.dart';
+export 'src/state/component/scroll_component.dart';
+
+export 'src/device/device_type.dart';
+
+export 'src/color_helper.dart';
