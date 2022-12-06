0488fde151ee4a88bdae987b4b874b69b238bb88
o <omnesiaorg@gmail.com>
2022-12-06 11:59:31 +0100

    Adapt example from BuildOwner instead since it doesnt take one frame

--- lib/src/widget/measure_util.dart
+++ lib/src/widget/measure_util.dart
@@ -1,68 +1,47 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'overlay_manager.dart';
-import 'post_frame/render_helper.dart';
+import 'package:flutter/rendering.dart';
 
 /// Small utility to measure a widget before actually putting it on screen.
 ///
 /// This can be helpful e.g. for positioning context menus based on the size they will take up.
 ///
-/// NOTE: This will take one frame!
-/// To circumvent a one-frame delay, you could e.g. request the measurement before you
-/// start an opening animation with estimated target size, which is adjusted in the next frame
-/// once the measurement result is available.
-///
 /// NOTE: Use sparingly, since this takes a complete layout and sizing pass for the subtree you
 /// want to measure.
 ///
-/// Uses an invisible overlay with optional [constraints] (defaults to unconstraint)
+/// Compare https://api.flutter.dev/flutter/widgets/BuildOwner-class.html
 class MeasureUtil {
-  final BoxConstraints? constraints;
-  late final OverlayManager manager;
-  late final GlobalKey key;
-  bool _isDisposed = false;
-  bool get isDisposed => _isDisposed;
+  Size measureWidget(Widget widget, [BoxConstraints constraints = const BoxConstraints()]) {
+    final PipelineOwner pipelineOwner = PipelineOwner();
+    final _MeasurementView rootView = pipelineOwner.rootNode = _MeasurementView(constraints);
+    final BuildOwner buildOwner = BuildOwner(focusManager: FocusManager());
+    final RenderObjectToWidgetElement<RenderBox> element = RenderObjectToWidgetAdapter<RenderBox>(
+      container: rootView,
+      debugShortDescription: '[root]',
+      child: widget,
+    ).attachToRenderTree(buildOwner);
+    try {
+      rootView.scheduleInitialLayout();
+      pipelineOwner.flushLayout();
+      return rootView.size;
+    } finally {
+      // Clean up.
+      element.update(RenderObjectToWidgetAdapter<RenderBox>(container: rootView));
+      buildOwner.finalizeTree();
+    }
+  }
+}
 
-  MeasureUtil({
-    required Widget Function(BuildContext context) builder,
-    this.constraints,
-  }) {
-    this.key = GlobalKey(debugLabel: "Measure Util Key");
-    this.manager = OverlayManager(
-      overlayBuilder: (context) => Center(
-        child: OverflowBox(
-          maxHeight: constraints?.maxWidth ?? double.infinity,
-          maxWidth: constraints?.maxWidth ?? double.infinity,
-          minHeight: constraints?.minHeight,
-          minWidth: constraints?.minWidth,
-          child: Opacity(
-            opacity: 0,
-            child: KeyedSubtree(
-              key: key,
-              child: builder(context),
-            ),
-          ),
-        ),
-      ),
-    );
+class _MeasurementView extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
+  final BoxConstraints boxConstraints;
+  _MeasurementView(this.boxConstraints);
+
+  @override
+  void performLayout() {
+    assert(child != null);
+    child!.layout(boxConstraints, parentUsesSize: true);
+    size = child!.size;
   }
 
-  void dispose() {
-    manager.dispose();
-    _isDisposed = true;
-  }
-
-  Future<Size?> measure(BuildContext context) async {
-    assert(!_isDisposed, "Dont call measure after dispose");
-
-    await manager.show(context);
-
-    // wait one frame
-    await RenderHelper.nextFrameFuture;
-
-    final size = RenderHelper.getSize(globalKey: this.key);
-    manager.hide(context);
-    return size;
-  }
+  @override
+  void debugAssertDoesMeetConstraints() => true;
 }
