7922e34d0f49870f309d2071b111bdc144600bae
ltOgt <omnesiaorg@gmail.com>
2023-01-19 01:22:10 +0100

    add custom decoration showing distance

--- example/lib/example/decorated_scrollable.dart
+++ example/lib/example/decorated_scrollable.dart
@@ -41,7 +41,45 @@ class _DecoratedScrollableExampleState extends State<DecoratedScrollableExample>
               ),
             ),
           ),
+          buildDecoration: (v, h) {
+            return Stack(
+              children: [
+                Positioned.fill(child: DecoratedScrollable.buildDecorationDefault(v, h)),
+                if (v != null) ...[
+                  PositionedOnEdgeX.top(_Circle(distance: v.metrics.extentBefore)),
+                  PositionedOnEdgeX.bottom(_Circle(distance: v.metrics.extentAfter)),
+                ],
+                if (h != null) ...[
+                  PositionedOnEdgeX.left(_Circle(distance: h.metrics.extentBefore)),
+                  PositionedOnEdgeX.right(_Circle(distance: h.metrics.extentAfter)),
+                ],
+              ],
+            );
+          },
         ),
+      ),
+    );
+  }
+}
+
+class _Circle extends StatelessWidget {
+  const _Circle({Key? key, required this.distance}) : super(key: key);
+
+  final double distance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        width: 40,
+        height: 40,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black, blurRadius: 1, spreadRadius: 1, offset: Offset(1, 1))],
+          shape: BoxShape.circle,
+        ),
+        child: AutoSizeText(text: "${distance.round()}"),
       ),
     );
   }
