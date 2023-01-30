7ef401ee769fd5dbbc646919d1fe5edc80e19d1a
ltOgt <omnesiaorg@gmail.com>
2023-01-30 14:12:46 +0100

    optional existing focusNode

--- lib/src/widget/builder/focus_builder.dart
+++ lib/src/widget/builder/focus_builder.dart
@@ -5,25 +5,58 @@ class FocusBuilder extends StatefulWidget {
   const FocusBuilder({
     Key? key,
     required this.builder,
+    this.focusNode,
   }) : super(key: key);
 
   final Widget Function(FocusNode focusNode) builder;
+  final FocusNode? focusNode;
 
   @override
   State<FocusBuilder> createState() => _FocusBuilderState();
 }
 
 class _FocusBuilderState extends ComponentState<FocusBuilder> {
-  late StateComponentFocus focus = StateComponentFocus(
-    state: this,
-    setStateOnChange: true,
-  );
+  late FocusNode focusNode;
+
+  void _update() => setState(() {});
+
+  _init() {
+    focusNode = widget.focusNode ?? FocusNode();
+    focusNode.addListener(_update);
+  }
+
+  _dispose(FocusBuilder widget) {
+    focusNode.removeListener(_update);
+    if (widget.focusNode == null) {
+      focusNode.dispose();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  void didUpdateWidget(covariant FocusBuilder oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    _dispose(oldWidget);
+    _init();
+  }
+
+  @override
+  void dispose() {
+    _dispose(widget);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Focus(
-      focusNode: focus.obj,
-      child: widget.builder(focus.obj),
+      focusNode: focusNode,
+      child: widget.builder(focusNode),
     );
   }
 }
