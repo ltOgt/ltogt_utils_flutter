46d0e92e303dd42821e2e2ea93bb610bcbd85f42
ltOgt <omnesiaorg@gmail.com>
2024-05-26 23:51:38 +0200

    add PositionedOnEdgeX.fromAlignment

--- lib/src/extensions/positioned_on_edge_x.dart
+++ lib/src/extensions/positioned_on_edge_x.dart
@@ -2,6 +2,44 @@ import 'package:flutter/widgets.dart';
 import 'package:ltogt_utils_flutter/src/widget/positioned_maybe_animated.dart';
 
 extension PositionedOnEdgeX on Positioned {
+  static PositionedMaybeAnimated fromAlignment({
+    required Alignment alignment,
+    required Widget child,
+    Duration? duration,
+    Key? key,
+    double delta = 0,
+  }) {
+    assert([-1.0, 0, 1].contains(alignment.x));
+    assert([-1.0, 0, 1].contains(alignment.y));
+
+    switch (alignment) {
+      case Alignment.topLeft:
+        return topLeft(child: child, deltaLeft: delta, deltaTop: delta, duration: duration);
+      case Alignment.topCenter:
+        return top(child: child, delta: delta, duration: duration);
+      case Alignment.topRight:
+        return topRight(child: child, deltaRight: delta, deltaTop: delta, duration: duration);
+      case Alignment.centerLeft:
+        return left(child: child, delta: delta, duration: duration);
+
+      case Alignment.centerRight:
+        return right(child: child, delta: delta, duration: duration);
+      case Alignment.bottomLeft:
+        return bottomLeft(child: child, deltaLeft: delta, deltaBottom: delta, duration: duration);
+      case Alignment.bottomCenter:
+        return bottom(child: child, delta: delta, duration: duration);
+      case Alignment.bottomRight:
+        return bottomRight(child: child, deltaRight: delta, deltaBottom: delta, duration: duration);
+      case Alignment.center:
+      case _:
+        return PositionedMaybeAnimated(
+          keyX: key,
+          durationForAnimation: duration,
+          child: child,
+        );
+    }
+  }
+
   static PositionedMaybeAnimated top({required Widget child, Duration? duration, Key? key, double delta = 0}) =>
       PositionedMaybeAnimated(
         keyX: key,
